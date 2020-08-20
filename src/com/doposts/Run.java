08c6b0e3ab345e7a634ad3981dc21dc003a3d5ac
dx_hualuo <2020547773@qq.com>
2020-08-20 15:48:02 +0800

    更新类库

--- src/com/doposts/Run.java
+++ src/com/doposts/Run.java
@@ -13,10 +13,6 @@ import java.util.List;
 @SuppressWarnings("RedundantThrows")
 public class Run {
     public static void main(String[] args) throws Exception{
-        PostClass postClass = new PostClass();
-        postClass.setClassFatherId(12);
-        postClass.setClassLevel(3);
-        postClass.setClassName("4399小游戏");
-        System.out.println(PostItDatabase.POST_CLASS_DAO.insertPostClass(postClass));
+        System.out.println(PostItDatabase.POST_CLASS_DAO.findPostClassByClassName("4399小游戏"));
     }
 }
