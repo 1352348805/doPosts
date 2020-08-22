d48a5983ba3435bedab01ec0acef7e7df4a3123f
dx_hualuo <2020547773@qq.com>
2020-08-22 11:06:52 +0800

    更新类库解决Count未关连接

--- src/com/doposts/Run.java
+++ src/com/doposts/Run.java
@@ -14,6 +14,19 @@ import static com.doposts.dao.PostItDatabase.DEFAULT_DAO;
 @SuppressWarnings("RedundantThrows")
 public class Run {
     public static void main(String[] args) throws Exception{
-        System.out.println(PostItDatabase.CREATE_CLASS_REQUEST_DAO.selectAllCreateClassRequestByCondition(0, 5));
+        for (int i = 0; i < 500; i++) {
+            System.out.println(i+1);
+            System.out.println(PostItDatabase.USER_DAO.selectUserCount());
+            System.out.println(PostItDatabase.USER_DAO.selectUserByStartIndexAndLength(0, 5));
+            System.out.println(PostItDatabase.USER_DAO.selectUserCount());
+            System.out.println(PostItDatabase.USER_DAO.selectUserByStartIndexAndLength(5, 5));
+            System.out.println(PostItDatabase.USER_DAO.selectUserCount());
+            System.out.println(PostItDatabase.USER_DAO.selectUserByStartIndexAndLength(10, 5));
+            System.out.println(PostItDatabase.USER_DAO.selectUserCount());
+            System.out.println(PostItDatabase.USER_DAO.selectUserByStartIndexAndLength(15, 5));
+            System.out.println(PostItDatabase.USER_DAO.selectUserCount());
+            System.out.println(PostItDatabase.USER_DAO.selectUserByStartIndexAndLength(20, 5));
+        }
+
     }
 }
