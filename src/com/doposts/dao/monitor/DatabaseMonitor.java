86194d677c65d1c7833f4a146945a41e3aa41c2f
dx_hualuo <2020547773@qq.com>
2020-08-25 12:25:04 +0800

    类库     StringStream更新，Array更新 项目     DAO层异常日志监控，和可视化输出

--- src/com/doposts/dao/monitor/DatabaseMonitor.java
+++ src/com/doposts/dao/monitor/DatabaseMonitor.java
@@ -23,6 +23,10 @@ public class DatabaseMonitor {
      *  存放DAO层接口日志信息的
      */
     private final MappingList<String, InterfaceLogItem> interfaceLog = new MappingList<>();
+    /**
+     *  存放DAO层报错日志信息的
+     */
+    private final MappingList<String, InterfaceLogItem> interfaceLogError = new MappingList<>();
 
     /**
      *  记录Interface被调用的日志
@@ -37,10 +41,18 @@ public class DatabaseMonitor {
         printLog(interfaceName, log);
     }
 
+    public static void logErrorInterface(String interfaceName, InterfaceLogItem log){
+        if(monitor == null){
+            return;
+        }
+        monitor.interfaceLogError.put(interfaceName, log);
+        printErrorLog(interfaceName, log);
+    }
+
     private static void printLog(String interfaceName, InterfaceLogItem log) {
         if(output){
             StringBuilder builder = new StringBuilder();
-            builder.append("                           ============ DX_Database ============ \n\t数据库调用：").append(interfaceName).append("\n");
+            builder.append("                           ============ DX_Database ============ \n\t数据库接口调用：").append(interfaceName).append("\n");
             builder.append("\t方法名：").append(log.getMethodName()).append("\n");
             builder.append("\t参数：\n").append(log.getParams());
             builder.append("\t调用位置：").append(log.getLocation()).append("\n");
@@ -49,8 +61,43 @@ public class DatabaseMonitor {
             }
             builder.append("\t耗时：").append(log.getMs()).append("ms").append("\n");
             builder.append("\t时间：").append(log.getTime()).append("\n");
-            builder.append("==============================================================================================");
+            builder.append("=============================================================================================");
             System.out.println(builder);
         }
+    }
+
+    private static void printErrorLog(String interfaceName, InterfaceLogItem log){
+        if(output){
+            System.out.println("===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===");
+            System.out.println("                             ======= DX_Database 异常信息 =======   ");
+            StringBuilder builder = new StringBuilder();
+            builder.append("\t数据库接口调用：").append(interfaceName).append("\n");
+            builder.append("\t方法名：").append(log.getMethodName()).append("\n");
+            builder.append("\t参数：\n\t\t").append(log.getParams() == null ? null+ "\n": log.getParams());
+            builder.append("\t调用位置：").append(log.getLocation()).append("\n");
+            builder.append("\t异常信息：↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ \n");
+            System.out.print(builder);
+            builder.setLength(0);
+            System.out.print("\t\t"+log.getData());
+            builder.append("\t时间：").append(log.getTime());
+            System.out.println(builder);
+            System.out.println("===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===WARNING===");
+        }
+    }
+
+    /**
+     *  获得所有的执行异常的接口日志
+     * @return 接口日志
+     */
+    public MappingList<String, InterfaceLogItem> getInterfaceLogError() {
+        return interfaceLogError;
+    }
+
+    /**
+     *  获得所有的正常执行的接口日志
+     * @return 接口日志
+     */
+    public MappingList<String, InterfaceLogItem> getInterfaceLog() {
+        return interfaceLog;
     }
 }
